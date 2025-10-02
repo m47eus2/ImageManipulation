9313091726a87009ef37341787d10ad1bb40a997
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2025-10-02 16:17:38 +0200

    Add lab1

---
+++ lab1/img.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import sys
+import numpy as np
+ 
+img = cv.imread("lab1/panZeStocka.jpg")
+
+print(np.shape(img))
+print(img[100,100])
+img[100:200, 100:200] = (255,0,0)
+roi = img[500:600,500:600]
+img[200:300, 200:300] = roi
+img3 = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+b,g,r = cv.split(img)
+#cv.imshow("Display window", img3)
+cv.imshow("Channel b", b)
+cv.imshow("Channel g", g)
+cv.imshow("Channel r", r)
+cv.waitKey(0)
